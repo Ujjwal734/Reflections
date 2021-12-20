9a7550e4faf47f0d32d62d218321aa5b1713ef1f
Ujjwal734 <ujjwalvishwakarma734@gmail.com>
2021-12-20 08:00:49 +0530

    Reflection string type

---
+++ Reflections/Reflection1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflections
+{
+    public class Reflection1
+    {
+        public static void Main(string[] args)
+        {
+            Type t = typeof(string);
+
+            Console.WriteLine("Ujjwal :- {0}", t.Name);
+            Console.WriteLine("Ujjwal Vishwakarma :- {0}", t.FullName);
+            Console.WriteLine("Base Type :- {0}", t.BaseType);
+        }
+    }
+}
